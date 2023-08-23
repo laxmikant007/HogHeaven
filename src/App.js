d475bb8361e0269edf0620b5a58737536767b551
Laxmikant Saraswat <laxmikantsarswat007@gmail.com>
2023-08-23 11:54:58 +0530

    Aura Girls

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import Footer from './components/Footer';
+import Header from './components/Header';
+import About from './pages/About';
+import Contact from './pages/Conteact';
+import Home from './pages/Home';
+
+import Facilities from './pages/Our-fecalities';
+import Team from './pages/Our-team';
+import Testimonials from './pages/Testimonial';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+
+function App() {
+  return (
+     <Router>
+      <div>
+      <Header/>
+     
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/team' element={<Team/>}/>
+        <Route path='/facility' element={<Facilities/>}/>
+        <Route path='/testimonials' element={<Testimonials/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
+      <Footer/>
+    
+      </div>
+      </Router>
+    
+  );
+}
+
+export default App;
